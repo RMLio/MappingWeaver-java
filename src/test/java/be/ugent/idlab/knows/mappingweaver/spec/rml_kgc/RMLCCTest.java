b04e231490ae2905c303c9e0fe775d4acea5038c
Matthieu De Clercq <matthieudeclercq2310@gmail.com>
2026-07-08 23:05:54 +0200

    RMLCCTest.java updated to all tests

--- src/test/java/be/ugent/idlab/knows/mappingweaver/spec/rml_kgc/RMLCCTest.java
+++ src/test/java/be/ugent/idlab/knows/mappingweaver/spec/rml_kgc/RMLCCTest.java
@@ -1,75 +1,71 @@
 package be.ugent.idlab.knows.mappingweaver.spec.rml_kgc;
 
 import be.ugent.idlab.knows.mappingweaver.cores.TestCore;
-import be.ugent.idlab.knows.mappingweaver.utilities.FlinkMiniClusterExtension;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
 import java.util.stream.Stream;
 
-@Disabled
 public class RMLCCTest extends TestCore {
-    private static final List<String> testsFailed = List.of(
-            //// positive, rust panic
-            // Predicate Object Map BlankNode(BnodeId("riog00000003")) has 0 object maps
-            // waiting for https://gitlab.ilabt.imec.be/rml/proc/algemaploom-rs/-/issues/16
-            "RMLTC-CC-0001-Alt",
-            "RMLTC-CC-0001-Bag",
-            "RMLTC-CC-0001-List",
-            "RMLTC-CC-0001-Seq",
-            "RMLTC-CC-0003-EB",
-            "RMLTC-CC-0003-EL",
-            "RMLTC-CC-0003-NEB",
-            "RMLTC-CC-0003-NEL",
-            "RMLTC-CC-0003-NELb",
-            "RMLTC-CC-0005-App1",
-            "RMLTC-CC-0005-App2",
-            "RMLTC-CC-0005-Car1",
-            "RMLTC-CC-0005-Car2",
-            "RMLTC-CC-0006-IT0",
-            "RMLTC-CC-0006-IT3",
-            "RMLTC-CC-0007-NES",
-            "RMLTC-CC-0009-DUP-Bag",
-            "RMLTC-CC-0009-DUP-List",
-            "RMLTC-CC-0010-Lista",
 
-            // Cannot parse term map type for BlankNode(BnodeId("riog00000002"))
-            // waiting for https://gitlab.ilabt.imec.be/rml/proc/algemaploom-rs/-/issues/17
-            "RMLTC-CC-0004-SM1",
-            "RMLTC-CC-0004-SM2",
-            "RMLTC-CC-0004-SM3",
-            "RMLTC-CC-0004-SM4",
-            "RMLTC-CC-0004-SM5",
-
-            // positive, Expected output path does not exist! --> Fix in TestCore (default.nq should also be allowed)
-            // waiting for https://gitlab.ilabt.imec.be/rml/proc/algemaploom-java/-/issues/17
-            "RMLTC-CC-0002-Bag",
-            "RMLTC-CC-0002-List",
-            "RMLTC-CC-0003-EL-BN",
-            "RMLTC-CC-0003-EL-Named",
-            "RMLTC-CC-0006-IT1",
-            "RMLTC-CC-0006-IT2",
-            "RMLTC-CC-0006-IT4",
-            "RMLTC-CC-0006-IT5",
-            "RMLTC-CC-0008-ROMa",
-            "RMLTC-CC-0008-ROMb",
-            "RMLTC-CC-0010-Listb"
-
-    );
-
-    private static Stream<Arguments> positiveTests() {
-        return Stream.of("").map(Arguments::of);
+    public static String getModule() {
+        return "rml_kgc/rml-cc";
     }
 
-    @Disabled("Does not yet work")
-    @ParameterizedTest(name = "Index: {index} Filename: {0}")
-    @MethodSource("positiveTests")
-    public void positiveTest(String directory) throws Exception {
-        this.positiveTest("src/test/resources/spec/rml_kgc/rml-cc/", directory);
+    // All RML-CC test cases are positive (README: "**Error expected?** No"), but none
+    // currently pass: RML Collections & Containers (rml:gather / rml:gatherAs) is not
+    // implemented. 24 cases crash while translating the gather, the other 11 run but
+    // silently drop the gathered collection/container (rdf:Alt/Bag/Seq and rdf:List),
+    // so their output does not match the expected result.
+    private static Stream<Arguments> positiveFailing() {
+        return Stream.of(
+                // Crash: translation of rml:gather panics, no output is produced
+                "RMLTC-CC-0001-Alt",
+                "RMLTC-CC-0001-Bag",
+                "RMLTC-CC-0001-List",
+                "RMLTC-CC-0001-Seq",
+                "RMLTC-CC-0003-EB",
+                "RMLTC-CC-0003-EL",
+                "RMLTC-CC-0003-NEB",
+                "RMLTC-CC-0003-NEL",
+                "RMLTC-CC-0003-NELb",
+                "RMLTC-CC-0004-SM1",
+                "RMLTC-CC-0004-SM2",
+                "RMLTC-CC-0004-SM3",
+                "RMLTC-CC-0004-SM4",
+                "RMLTC-CC-0004-SM5",
+                "RMLTC-CC-0005-App1",
+                "RMLTC-CC-0005-App2",
+                "RMLTC-CC-0005-Car1",
+                "RMLTC-CC-0005-Car2",
+                "RMLTC-CC-0006-IT0",
+                "RMLTC-CC-0006-IT3",
+                "RMLTC-CC-0007-NES",
+                "RMLTC-CC-0009-DUP-Bag",
+                "RMLTC-CC-0009-DUP-List",
+                "RMLTC-CC-0010-Lista",
+
+                // Wrong output: runs, but the gathered collection/container isthrown away, so only the link triples are produced
+                "RMLTC-CC-0002-Bag",
+                "RMLTC-CC-0002-List",
+                "RMLTC-CC-0003-EL-BN",
+                "RMLTC-CC-0003-EL-Named",
+                "RMLTC-CC-0006-IT1",
+                "RMLTC-CC-0006-IT2",
+                "RMLTC-CC-0006-IT4",
+                "RMLTC-CC-0006-IT5",
+                "RMLTC-CC-0008-ROMa",
+                "RMLTC-CC-0008-ROMb",
+                "RMLTC-CC-0010-Listb"
+        ).map(Arguments::of);
+    }
+
+    @Disabled("Not running known failing test cases in CI")
+    @ParameterizedTest(name = "Positive test index: {index} Filename: {0}")
+    @MethodSource("positiveFailing")
+    public void positiveFailingTest(String directory) throws Exception {
+        super.positiveTest("src/test/resources/spec/rml_kgc/rml-cc/", directory + '/');
     }
 }
-
